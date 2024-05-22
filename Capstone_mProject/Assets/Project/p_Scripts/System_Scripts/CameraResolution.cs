9b7fb37f8a04a97302758a5ff31b1b928cc8a611
hyhy02 <sss601301@naver.com>
2024-05-22 21:45:45 +0900

    - 해상도 대응 오류수정, 이펙트 쉐이더 오류 수정

--- Capstone_mProject/Assets/Project/p_Scripts/System_Scripts/CameraResolution.cs
+++ Capstone_mProject/Assets/Project/p_Scripts/System_Scripts/CameraResolution.cs
@@ -15,42 +15,35 @@ public class CameraResolution : MonoBehaviour
     {
         canvasScaler = GetComponent<CanvasScaler>();
         SetResolution(); // 초기에 게임 해상도 고정
+        if (canvasScaler != null)
+        {
+            FixScales();
+        }
+        
     }
 
     /* 해상도 설정하는 함수 */
     public void SetResolution()
     {
+        canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+
         if (canvasScaler == null)
             canvasScaler = GetComponent<CanvasScaler>();
 
         int deviceWidth = Screen.width; // 기기 너비 저장
         int deviceHeight = Screen.height; // 기기 높이 저장
 
-        if (canvasScaler.matchWidthOrHeight == 1)
-        {
-            if (deviceWidth < setWidth)
-            {
-                canvasScaler.matchWidthOrHeight = 0;
-            }
-            else
-            {
-                canvasScaler.matchWidthOrHeight = 1;
-            }
+                //Default 해상도 비율
+        float fixedAspectRatio = setWidth / setHeight;
 
-        }
-        else if (canvasScaler.matchWidthOrHeight == 0)
-        {
+        //현재 해상도의 비율
+        float currentAspectRatio = (float)Screen.width / (float)Screen.height;
 
-            if (deviceHeight < setHeight)
-            {
-                canvasScaler.matchWidthOrHeight = 0;
-            }
-            else
-            {
-                canvasScaler.matchWidthOrHeight = 1;
-            }
-
-        }
+        Debug.Log($"{currentAspectRatio}            {fixedAspectRatio}");
+        //현재 해상도 가로 비율이 더 길 경우
+        // if (currentAspectRatio > fixedAspectRatio) canvasScaler.matchWidthOrHeight = 1;
+        // //현재 해상도의 세로 비율이 더 길 경우
+        // else if (currentAspectRatio < fixedAspectRatio) canvasScaler.matchWidthOrHeight = 0;
 
 
         Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution 함수 제대로 사용하기
@@ -66,6 +59,24 @@ public class CameraResolution : MonoBehaviour
             Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
         }
     }
+    public void FixScales()
+    {
+        foreach (Transform child in transform)
+        {
+            FixScaleRecursive(child);
+        }
+    }
+
+    private void FixScaleRecursive(Transform obj)
+    {
+        obj.localScale = Vector3.one; // scale을 1로 설정
+        // foreach (Transform child in obj)
+        // {
+        //     FixScaleRecursive(child); // 자식들에 대해 재귀적으로 호출
+        // }
+    }
+
+
 
 
 }
