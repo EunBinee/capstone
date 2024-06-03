af4560fef0c53e42bdfac947b0ae68333ab9a3e1
minji411 <71704392+minji411@users.noreply.github.com>
2024-06-03 20:14:34 +0900

    - 플레이어 피격 시 빨간맛

--- Capstone_mProject/Assets/Project/p_Scripts/Character_Scripts/Player_Scripts/PlayerDefinitions.cs
+++ Capstone_mProject/Assets/Project/p_Scripts/Character_Scripts/Player_Scripts/PlayerDefinitions.cs
@@ -19,6 +19,8 @@ public class PlayerComponents
     public Transform playerHeadPos;
     public Transform playerBackPos;
     public Material sickScreen;
+    public List<SkinnedMeshRenderer> skinnedMeshRenderers;
+    public Material hitMat;
 }
 
 [Serializable]
