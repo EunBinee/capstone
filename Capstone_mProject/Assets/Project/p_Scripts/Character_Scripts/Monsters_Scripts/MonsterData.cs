495179b2fea94b11c8a5a60c7408ffd2df16684d
hyhy02 <sss601301@naver.com>
2024-10-09 00:50:48 +0900

    - 데미지 수정

--- Capstone_mProject/Assets/Project/p_Scripts/Character_Scripts/Monsters_Scripts/MonsterData.cs
+++ Capstone_mProject/Assets/Project/p_Scripts/Character_Scripts/Monsters_Scripts/MonsterData.cs
@@ -34,6 +34,7 @@ public class MonsterData
     public bool haveLastWeakness = false; //약점 끝나고, 마지막 약점
     public List<Transform> lastWeaknessList;
     public float weaknessDamageRate = 0.05f; //* 몬스터의 MaxHP의 5% 정도의 데미지를 주겠다는 뜻
+    public int weaknessHP_ = 15; //약점 체력
 
     [Header("몬스터 체력")]
     public Transform HPBarPos;
